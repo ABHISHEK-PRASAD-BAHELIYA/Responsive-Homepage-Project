7f9deec1443229d2047b7112fe1f9ff5cbd970e9
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-10-14 09:23:17 +0530

    finish header section

---
+++ src/header.js
@@ -0,0 +1,35 @@
+
+export const header = `
+<header class="header">
+  <div class="style-wrapper">
+    <div class="style-layer red skew box-shadow">
+      <div class="style-layer orange anim-layer">
+        <div class="style-layer yellow anim-layer">
+          <div class="style-layer green anim-layer">
+            <div class="style-layer blue anim-layer">
+              <div class="style-layer purple anim-layer">
+                <div class="style-layer blue-main anim-layer"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="wrapper">
+    <div class="img-wrapper">
+      <img src="imgs/jolyne-header.jpg" alt="">
+      <h1>JOLYNE CUJOH</h1>
+    </div>
+    <div class="about-me">
+      <h2>About Me</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit...</p>
+      <div class="social-icons">
+        <a href="#" class="fa fa-github"></a>
+        <a href="#" class="fa fa-linkedin"></a>
+        <a href="#" class="fa fa-twitter"></a>
+      </div>
+    </div>
+  </div>
+</header>
+`;
