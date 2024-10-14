298e5ddd01485d433d8bffa92a002fe384ac94ff
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-10-14 09:24:15 +0530

    finish main  section

---
+++ src/mainSection.js
@@ -0,0 +1,21 @@
+export const mainSection = `
+<section class="battle-section">
+  <div class="wrapper">
+    <h2>My Battles</h2>
+    <div class="battles">
+      <div class="battle-div box-shadow">
+        <img src="imgs/gwess.jpg" alt="">
+        <div class="battle-info">
+          <div class="title">
+            <h3>vs. GWESS</h3>
+            <a href="https://jojowiki.com/SO_Episode_2" target="_blank" class="fa fa-external-link"></a>
+          </div>
+          <h4>「Goo Goo Dolls」</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+        </div>
+      </div>
+      <!-- Add other battle items similarly -->
+    </div>
+  </div>
+</section>
+`;
