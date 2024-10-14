7dc19da9b73ee21fff9d06e1834cd333e4d77d23
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-10-14 20:13:01 +0530

    Deployment commit

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg|jpeg|woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
+                type: 'assets/resource',
             },
         ],
     },
