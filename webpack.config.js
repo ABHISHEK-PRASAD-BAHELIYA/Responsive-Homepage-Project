df04ba359e293f54444c2af2260a3c07582a1cb5
ABHISHEK-PRASAD-BAHELIYA <singhabhishek74401@gmail.com>
2024-10-14 20:29:11 +0530

    finish project and solve error

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg|jpeg|woff|woff2|eot|ttf|otf)$/i,
-                type: 'assets/resource',
+                type: 'asset/resource',
             },
         ],
     },
